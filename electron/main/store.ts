2d255cd10d5bb73be44ba6b63c31d417075198b1
snekcode <code.snakes@gmail.com>
2025-02-11 20:41:45 -0600

    fixed config file empty condition

--- electron/main/store.ts
+++ electron/main/store.ts
@@ -2,9 +2,14 @@ import Store from "electron-store";
 import { IStore } from "~/Shared/store";
 
 export const store = new Store<IStore>({
+  // @ts-expect-error - This is a valid path
+  defaults: {
+    entries: []
+    // Add other required properties with default values
+  },
   migrations: {
     "2.1.0": (store) => {
-        const data = store.get('entries');
+        const data = store.get('entries', []);
         const newData = data.map((entry: any) => ({ ...entry, enabled: false }));
         store.set('entries', newData);
     },
